159bf5a1beaf4a7a139081dfce52abc4e79c6207
mefs <dennis.mefs@gmail.com>
2022-03-09 21:43:58 +0800

    Add start gap and feistel features

--- Memory.cpp
+++ Memory.cpp
@@ -1,10 +1,15 @@
 #include "Memory.h"
+#include <fstream>
 #include <iostream>
 #include "FeistelNetwork.h"
 #include "Line.h"
 
+#ifdef FEISTEL
+#define START_GAP
+#endif
+
 unsigned long Memory::num_line_ex_gap = NUM_LINES - 1;
-long Memory::trigger_times = 100;
+unsigned long Memory::trigger_times = 100;
 
 Memory::Memory() : start_line(0), gap_line(NUM_LINES - 1), write_count(0)
 {
@@ -44,32 +49,43 @@ unsigned long Memory::ia_to_pa(unsigned long inter_line_num)
     return result;
 }
 
+#ifdef FEISTEL
 unsigned long Memory::la_to_ia(unsigned long log_line_num)
 {
     return FeistelNetwork::cal(log_line_num);
 }
+#else
+unsigned long Memory::la_to_ia(unsigned long log_line_num)
+{
+    return log_line_num;
+}
+#endif
 
 /*
     Mapping of Logical Address to Physical Address.
 */
+#ifdef START_GAP
 unsigned long Memory::la_to_pa(unsigned long log_line_num)
 {
     unsigned long ia = la_to_ia(log_line_num);
     unsigned long pa = ia_to_pa(ia);
     return pa;
 }
+#else
+unsigned long Memory::la_to_pa(unsigned long log_line_num)
+{
+    return log_line_num;
+}
+#endif
+
 
 /*
     Update Memory info
 */
-bool Memory::write(long log_addr)
+bool Memory::write(unsigned long log_addr)
 {
-    if (log_addr < 0) {
-        std::cerr << "Error: log_addr < 0 in " << __func__ << std::endl;
-        return false;
-    }
-    unsigned long log_lnum = ((unsigned long) log_addr >> LINE_SIZE_BITS);
-    long phy_lnum = la_to_pa(log_lnum);
+    unsigned long log_lnum = (log_addr >> LINE_SIZE_BITS);
+    unsigned long phy_lnum = la_to_pa(log_lnum);
     array[phy_lnum].write();
 
     write_count++;
@@ -85,4 +101,17 @@ bool Memory::copy_line(unsigned long from_line_num, unsigned long to_line_num)
     // no need to read now
     array[to_line_num].write();
     return true;
+}
+
+void Memory::print()
+{
+    std::ofstream outfile("line_count.txt");
+}
+
+std::ostream &operator<<(std::ostream &strm, const Memory &mem)
+{
+    for (size_t i = 0; i < NUM_LINES; ++i) {
+        strm << mem.array[i].read() << std::endl;
+    }
+    return strm;
 }
